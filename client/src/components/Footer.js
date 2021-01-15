59cfcaa48bde89935beb5ec86afa91000c5949cd
Sasha <khovinalexa@gmail.com>
2021-01-14 21:57:08 -0800

    header + footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Grid, Container } from '@material-ui/core'
+
+const Footer = () => {
+    return (
+        <Container> <Grid container >
+            <Grid item xs={12} align='center'>Copyright &copy; ProStore   </Grid>
+        </Grid></Container>
+    )
+}
+
+export default Footer
