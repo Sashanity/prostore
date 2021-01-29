1f6d9b6e950e2230ea16454b0f36f8181f248d30
Sasha <khovinalexa@gmail.com>
2021-01-28 16:19:13 -0800

    admin create/update product

--- backend/handlers/adminAccessRoutesHandlers/adminProductsRoutesHandlers.js
+++ backend/handlers/adminAccessRoutesHandlers/adminProductsRoutesHandlers.js
@@ -17,3 +17,51 @@ export const deleteProductById = asyncHandler(async (req, res) => {
         throw new Error('Product not found')
     }
 })
+
+/*
+@desc    ADMIN Create product
+@route   POST /api/products
+@ access Private, Admin
+*/
+export const createNewProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Default name',
+        price: 0,
+        user: req.user._id, // admin id
+        image: '/images/sample.jpg',
+        brand: 'Default brand',
+        category: 'Default category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Default description',
+    })
+
+    const productCreated = await product.save()
+    res.status(201).json(productCreated)
+
+})
+
+/*
+@desc    ADMIN update product
+@route   PUT /api/products/:id
+@ access Private, Admin
+*/
+export const updateProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        product.name = req.body.name || product.name
+        product.price = req.body.price || product.price
+        product.description = req.body.description || product.description
+        product.image = req.body.image || product.image
+        product.brand = req.body.brand || product.brand
+        product.category = req.body.category || product.category
+        product.countInStock = req.body.countInStock || product.countInStock
+
+        const productUpdated = await product.save()
+        res.status(201).json(productUpdated)
+    } else {
+        res.status(404)
+        throw new Error('Product not Found')
+    }
+})
--- backend/routes/product_routes.js
+++ backend/routes/product_routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getProducts, getProduct } from '../handlers/productRoutesHandlers.js'
-import { deleteProductById } from '../handlers/adminAccessRoutesHandlers/adminProductsRoutesHandlers.js'
+import { deleteProductById, createNewProduct, updateProduct } from '../handlers/adminAccessRoutesHandlers/adminProductsRoutesHandlers.js'
 import { auth } from '../middleware/auth.js'
 import { adminAuth } from '../middleware/adminAuth.js'
 
@@ -8,9 +8,13 @@ const router = express.Router()
 
 // router.get('/', getProducts)
 // can use routse instead vv
-router.route('/').get(getProducts)
+router.route('/')
+    .get(getProducts)
+    .post(auth, adminAuth, createNewProduct)
+
 router.route('/:id')
     .get(getProduct)
     .delete(auth, adminAuth, deleteProductById)
+    .put(auth, adminAuth, updateProduct)
 
 export default router
