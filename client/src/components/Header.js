60325cb5de93bef999d5944ae7a93efadacae9f5
Sasha <khovinalexa@gmail.com>
2021-01-30 22:08:06 -0800

    better dropdown ui

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, NavLink } from 'react-router-dom'
 
-import { AppBar, Toolbar, Container, Typography, makeStyles, Box, Button, Menu, MenuItem, Fade } from '@material-ui/core'
+import { AppBar, Toolbar, Container, Typography, makeStyles, Box, Button, Menu, MenuItem, Fade, Popper, Grow, ClickAwayListener, Paper, MenuList } from '@material-ui/core'
 import { logoutUser } from '../actions/userActions';
 
 const useStyles = makeStyles((theme) => ({
@@ -25,29 +25,67 @@ export default function Header() {
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
-    const [anchorEl, setAnchorEl] = useState(null)
-    const [anchorElA, setAnchorElA] = useState(null);
+    const [openAdmin, setOpenAdmin] = useState(false)
+    const anchorRefAdmin = useRef(null)
+    const [openUser, setOpenUser] = useState(false)
+    const anchorRefUser = useRef(null)
 
-    const handleClickUser = (event) => {
-        setAnchorEl(event.currentTarget);
-        // console.log('NAVLINK CLICKED')
-    }
-
-    const handleClickAdmin = (event) => {
-        setAnchorElA(event.currentTarget);
-        // console.log('NAVLINK ADMIN CLICKED')
-    }
-
-    const handleClose = () => {
-        setAnchorEl(null)
-    }
-    const handleCloseA = () => {
-        setAnchorElA(null)
-    }
     const handleLogout = () => {
         dispatch(logoutUser())
-        setAnchorEl(null)
+        setOpenUser(null)
     }
+
+    const handleToggleUser = () => {
+        setOpenUser((prevOpenUser) => !prevOpenUser)
+    }
+
+    const handleToggleAdmin = () => {
+        setOpenAdmin((prevOpenAdmin) => !prevOpenAdmin)
+    }
+
+    const handleCloseAdmin = (e) => {
+        if (anchorRefAdmin.current && anchorRefAdmin.current.contains(e.target)) {
+            return
+        }
+        setOpenAdmin(false);
+    }
+    const handleCloseUser = (e) => {
+        if (anchorRefUser.current && anchorRefUser.current.contains(e.target)) {
+            return
+        }
+        setOpenUser(false);
+    }
+
+    function handleListKeyDown(e) {
+        if (e.key === 'Tab') {
+            e.preventDefault();
+            setOpenAdmin(false);
+        }
+    }
+    function handleListKeyDownUser(e) {
+        if (e.key === 'Tab') {
+            e.preventDefault();
+            setOpenUser(false);
+        }
+    }
+
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpenAdmin = useRef(openAdmin);
+    const prevOpenUser = useRef(openUser);
+    useEffect(() => {
+        if (prevOpenAdmin.current === true && openAdmin === false) {
+            anchorRefAdmin.current.focus()
+        }
+
+        prevOpenAdmin.current = openAdmin
+
+        if (prevOpenUser.current === true && openUser === false) {
+            anchorRefUser.current.focus()
+        }
+
+        prevOpenUser.current = openUser
+    }, [openAdmin, openUser])
 
     return (
         <div className={classes.root} >
@@ -61,10 +99,44 @@ export default function Header() {
                                 <Box>
                                     <Button
                                         style={{ color: '#969696' }}
-                                        onClick={handleClickUser}>
+                                        ref={anchorRefUser}
+                                        aria-controls={openUser ? 'menu-list-grow' : undefined}
+                                        aria-haspopup="true"
+                                        onClick={handleToggleUser}>
                                         {userInfo.name}<i className="fas fa-caret-down"></i>
                                     </Button>
-                                    <Menu
+                                    <Popper
+                                        open={openUser}
+                                        anchorEl={anchorRefUser.current}
+                                        role={undefined}
+                                        transition
+                                        disablePortal={false}
+                                        modifiers={{
+                                            flip: {
+                                                enabled: true,
+                                            },
+                                            preventOverflow: {
+                                                enabled: true,
+                                                boundariesElement: 'scrollParent',
+                                            }
+                                        }}>
+                                        {({ TransitionProps, placement }) => (
+                                            <Grow
+                                                {...TransitionProps}
+                                                style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                                            >
+                                                <Paper>
+                                                    <ClickAwayListener onClickAway={handleCloseUser}>
+                                                        <MenuList autoFocusItem={openUser} id="menu-list-grow" onKeyDown={handleListKeyDownUser}>
+                                                            <MenuItem onClick={handleCloseUser}><Link to='/profile'>PROFILE</Link></MenuItem>
+                                                            <MenuItem onClick={handleLogout}>LOGOUT</MenuItem>
+                                                        </MenuList>
+                                                    </ClickAwayListener>
+                                                </Paper>
+                                            </Grow>
+                                        )}
+                                    </Popper>
+                                    {/* <Menu
                                         id="menu-user"
                                         anchorEl={anchorEl}
                                         keepMounted
@@ -74,33 +146,57 @@ export default function Header() {
                                     >
                                         <MenuItem onClick={handleClose}><Link to='/profile'>PROFILE</Link></MenuItem>
                                         <MenuItem onClick={handleLogout}>LOGOUT</MenuItem>
-                                    </Menu>
+                                    </Menu> */}
+
                                 </Box>
                             )
                             : <Link to='/signin'><i className='fas fa-user'></i> SIGN IN </Link>
                         }
                         {userInfo && userInfo.isAdmin && (
-                            <Box>
+                            <>
                                 <Button
                                     style={{ color: '#969696' }}
-                                    onClick={handleClickAdmin}>
+                                    ref={anchorRefAdmin}
+                                    aria-controls={openAdmin ? 'menu-list-grow' : undefined}
+                                    aria-haspopup="true"
+                                    // onClick={handleClickAdmin}
+                                    onClick={handleToggleAdmin}
+                                >
                                     ADMIN <i className="fas fa-caret-down"></i>
                                 </Button>
-                                <Menu
-                                    id="menu-admin"
-                                    anchorEl={anchorElA}
-                                    keepMounted
-                                    open={Boolean(anchorElA)}
-                                    onClose={handleCloseA}
-                                    TransitionComponent={Fade}
-                                >
-                                    <MenuItem onClick={handleCloseA}><Link to='/admin/users'>USERS</Link></MenuItem>
-                                    <MenuItem onClick={handleCloseA}><Link to='/admin/orders'>ORDERS</Link></MenuItem>
-                                    <MenuItem onClick={handleCloseA}><Link to='/admin/products'>PRODUCTS</Link></MenuItem>
-
-
-                                </Menu>
-                            </Box>
+                                <Popper
+                                    open={openAdmin}
+                                    anchorEl={anchorRefAdmin.current}
+                                    role={undefined}
+                                    transition
+                                    disablePortal={false}
+                                    modifiers={{
+                                        flip: {
+                                            enabled: true,
+                                        },
+                                        preventOverflow: {
+                                            enabled: true,
+                                            boundariesElement: 'scrollParent',
+                                        }
+                                    }}>
+                                    {({ TransitionProps, placement }) => (
+                                        <Grow
+                                            {...TransitionProps}
+                                            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                                        >
+                                            <Paper>
+                                                <ClickAwayListener onClickAway={handleCloseAdmin}>
+                                                    <MenuList autoFocusItem={openAdmin} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                                        <MenuItem onClick={handleCloseAdmin}><Link to='/admin/users'>USERS</Link></MenuItem>
+                                                        <MenuItem onClick={handleCloseAdmin}><Link to='/admin/orders'>ORDERS</Link></MenuItem>
+                                                        <MenuItem onClick={handleCloseAdmin}><Link to='/admin/products'>PRODUCTS</Link></MenuItem>
+                                                    </MenuList>
+                                                </ClickAwayListener>
+                                            </Paper>
+                                        </Grow>
+                                    )}
+                                </Popper>
+                            </>
                         )
 
                         }
